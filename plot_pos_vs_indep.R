2fdb8a71e4f72216bbd3e16161096dcc2e4be12f
Philip Boeken <32740847+philipboeken@users.noreply.github.com>
2023-01-18 11:05:18 +0100

    update

--- plot_pos_vs_indep.R
+++ plot_pos_vs_indep.R
@@ -9,12 +9,10 @@ iter <- as.numeric(args[2])
 n <- as.numeric(args[3])
 
 pdf(sprintf("output/figures/pos_indep/figure_%s_%s_%s.pdf", graph_nr, iter, n), width = 15, height = 10)
-par(mfrow=c(3, 3))
-for(pos_mode in c("pos", "wpos", "npos")) {
-  for(indep_mode in c("indep", "wdep", "dep"))  {
+par(mfrow = c(3, 3))
+for (pos_mode in c("pos", "wpos", "npos")) {
+  for (indep_mode in c("indep", "wdep", "dep")) {
     print(experiment(graph_nr = graph_nr, iter = iter, n = n, pos_mode = pos_mode, indep_mode = indep_mode, plot_flag = TRUE))
   }
 }
 dev.off()
-
-
