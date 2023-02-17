2ab67ae46186c317b604d1bb04634f7e69cd3fdf
Philip Boeken <pboeken@hactar.science.uva.nl>
2023-02-17 11:54:35 +0100

    update

--- run_experiment1.sh
+++ run_experiment1.sh
@@ -10,23 +10,27 @@ JOBID0=$(sbatch --parsable exp1a_find_valid_graphs.sh)
 
 # 1b: Run experiments: n_iter=$NITER, n_obs=$NOBS, with positivity (P(S=1 | X, Z) > 0) and independence Y _||_S | X, Z:
 JOBID1=$(sbatch --parsable --dependency=afterok:$JOBID0 --array=1-$NITER exp1b_simulate_sbatch.sh $NOBS npos indep)
-JOBID2=$(sbatch --parsable --dependency=afterok:$JOBID0 --array=1-$NITER exp1b_simulate_sbatch.sh $NOBS pos indep)
-JOBID3=$(sbatch --parsable --dependency=afterok:$JOBID0 --array=1-$NITER exp1b_simulate_sbatch.sh $NOBS npos dep)
-JOBID4=$(sbatch --parsable --dependency=afterok:$JOBID0 --array=1-$NITER exp1b_simulate_sbatch.sh $NOBS pos dep)
+# JOBID2=$(sbatch --parsable --dependency=afterok:$JOBID0 --array=1-$NITER exp1b_simulate_sbatch.sh $NOBS pos indep)
+# JOBID3=$(sbatch --parsable --dependency=afterok:$JOBID0 --array=1-$NITER exp1b_simulate_sbatch.sh $NOBS npos dep)
+# JOBID4=$(sbatch --parsable --dependency=afterok:$JOBID0 --array=1-$NITER exp1b_simulate_sbatch.sh $NOBS pos dep)
 
 # 1c: Combine MSE results for results with n=$NOBS: -->
 sbatch --dependency=afterok:$JOBID0,afterany:$JOBID1 exp1c_process_results.sh $NITER $NOBS npos indep
-sbatch --dependency=afterok:$JOBID0,afterany:$JOBID2 exp1c_process_results.sh $NITER $NOBS pos indep
-sbatch --dependency=afterok:$JOBID0,afterany:$JOBID3 exp1c_process_results.sh $NITER $NOBS npos dep
-sbatch --dependency=afterok:$JOBID0,afterany:$JOBID4 exp1c_process_results.sh $NITER $NOBS pos dep
+# sbatch --dependency=afterok:$JOBID0,afterany:$JOBID2 exp1c_process_results.sh $NITER $NOBS pos indep
+# sbatch --dependency=afterok:$JOBID0,afterany:$JOBID3 exp1c_process_results.sh $NITER $NOBS npos dep
+# sbatch --dependency=afterok:$JOBID0,afterany:$JOBID4 exp1c_process_results.sh $NITER $NOBS pos dep
+# sbatch --dependency=afterok:$JOBID0 exp1c_process_results.sh $NITER $NOBS npos indep
+# sbatch --dependency=afterok:$JOBID0 exp1c_process_results.sh $NITER $NOBS pos indep
+# sbatch --dependency=afterok:$JOBID0 exp1c_process_results.sh $NITER $NOBS npos dep
+# sbatch --dependency=afterok:$JOBID0 exp1c_process_results.sh $NITER $NOBS pos dep
 
 # 1d: Make for each graph 25 plots with n=$NOBS:
-sbatch --dependency=afterok:$JOBID0 exp1d_plot_iterations_sbatch.sh 25 $NOBS pos indep
-sbatch --dependency=afterok:$JOBID0 exp1d_plot_iterations_sbatch.sh 25 $NOBS npos indep
-sbatch --dependency=afterok:$JOBID0 exp1d_plot_iterations_sbatch.sh 25 $NOBS pos dep
-sbatch --dependency=afterok:$JOBID0 exp1d_plot_iterations_sbatch.sh 25 $NOBS npos dep
+# sbatch --dependency=afterok:$JOBID0 exp1d_plot_iterations_sbatch.sh 5 $NOBS pos indep
+sbatch --dependency=afterok:$JOBID0 exp1d_plot_iterations_sbatch.sh 5 $NOBS npos indep
+# sbatch --dependency=afterok:$JOBID0 exp1d_plot_iterations_sbatch.sh 5 $NOBS pos dep
+# sbatch --dependency=afterok:$JOBID0 exp1d_plot_iterations_sbatch.sh 5 $NOBS npos dep
 
 # For graph_nr=1, plot results of a single iteration for all pos_mode and indep_mode values
-sbatch --dependency=afterok:$JOBID0 exp1e_compare_pos_indep.sh 1 1 $NOBS
-sbatch --dependency=afterok:$JOBID0 exp1e_compare_pos_indep.sh 19 1 $NOBS
-sbatch --dependency=afterok:$JOBID0 exp1e_compare_pos_indep.sh 41 1 $NOBS
+# sbatch --dependency=afterok:$JOBID0 exp1e_compare_pos_indep.sh 3 1 $NOBS
+# sbatch --dependency=afterok:$JOBID0 exp1e_compare_pos_indep.sh 19 1 $NOBS
+# sbatch --dependency=afterok:$JOBID0 exp1e_compare_pos_indep.sh 41 1 $NOBS
