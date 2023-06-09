31e6ab273c931cf68f406c5c6d04497503a8779c
Philip Boeken <philip.boeken@gmail.com>
2023-06-09 15:59:54 +0200

    update

--- exp1a_find_valid_graphs.sh
+++ exp1a_find_valid_graphs.sh
@@ -2,9 +2,9 @@
 #
 #SBATCH --job-name=exp1a_find_valid_graphs
 #SBATCH --open-mode=append
-#SBATCH --output=/home/<NAME>/debiased_regression/output/log/log_exp1a_find_valid_graphs.stdout
-#SBATCH --error=/home/<NAME>/debiased_regression/output/log/error_exp1a_find_valid_graphs.stderr
-#SBATCH --workdir=/home/<NAME>/debiased_regression
+#SBATCH --output=/home/pboeken/debiased_regression/output/log/log_exp1a_find_valid_graphs.stdout
+#SBATCH --error=/home/pboeken/debiased_regression/output/log/error_exp1a_find_valid_graphs.stderr
+#SBATCH --workdir=/home/pboeken/debiased_regression
 #SBATCH --time=10000:00
 #SBATCH --mem-per-cpu=1000
 #SBATCH --mail-type=END,FAIL,REQUEUE,TIME_LIMIT_80
